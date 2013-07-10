b3391be20bed768fe8d5d9730a0d4fce70d33813
Retina <Retina@Retinas-MacBook-Pro.local>
2013-07-10 02:08:59 +0200

    Part3 test1

--- grader.js
+++ grader.js
@@ -24,51 +24,74 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require("restler");
 
-var assertFileExists = function(infile) {
+var assertFileExists = function (infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
+    if (!fs.existsSync(instr)) {
+        console.log("%s file does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
+var cheerioHtmlFile = function (htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
+var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function (htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
+    for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
 };
 
-var clone = function(fn) {
+var clone = function (fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var fileChecks = function (file) {
+    var checkJson = checkHtmlFile(file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
+
+if (require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists))
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'Path to url')
+        .parse(process.argv);
+
+    if (program.file) {
+        console.log("went file branch");
+        console.log("File: " + program.file);
+        fileChecks(program.file);
+    } else {
+        console.log("went url branch");
+        console.log("URL: " + program.url);
+        rest.get(program.url).on('complete', function (result) {
+            if (result instanceof Error) {
+                console.log("%s url does not exist. Exiting.", program.url);
+                process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+            }
+            else {
+                fs.writeFileSync('remoteResult.html', result);
+                fileChecks('remoteResult.html');
+            }
+        });
+    }
 } else {
+    console.log("went else branch");
     exports.checkHtmlFile = checkHtmlFile;
 }
